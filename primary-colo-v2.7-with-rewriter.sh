effc276362687457b642abeedcb7842023fcc373
Wang Cheng <u3003676@connect.hku.hk>
2017-01-10 19:53:11 +0800

    Add files via upload

---
+++ primary-colo-v2.7-with-rewriter.sh
@@ -0,0 +1,15 @@
+echo "{'execute':'qmp_capabilities'}
+{'execute': 'human-monitor-command', 'arguments': {'command-line': 'drive_add -n buddy driver=replication,mode=primary,file.driver=nbd,file.host=3.3.3.8,file.port=8889,file.export=colo-disk0,node-name=node0'}}
+{'execute':'x-blockdev-change', 'arguments':{'parent': 'colo-disk0', 'node': 'node0' } }
+{'execute': 'migrate-set-capabilities', 'arguments': {'capabilities': [ {'capability': 'x-colo', 'state': true } ] } }
+{'execute': 'migrate', 'arguments': {'uri': 'tcp:3.3.3.8:8888' } }"
+echo "{'execute': 'trace-event-set-state', 'arguments': {'name': 'colo*', 'enable': true} }"
+
+disk_path=/home/cheng/ubuntu14.04.raw
+
+net_param="-netdev tap,id=hn0,vhost=off,script=/etc/qemu-ifup,downscript=/etc/qemu-ifdown -device e1000,id=e0,netdev=hn0 -chardev socket,id=mirror0,host=3.3.3.3,port=9003,server,nowait -chardev socket,id=compare1,host=3.3.3.3,port=9004,server,nowait -chardev socket,id=compare0,host=3.3.3.3,port=9001,server,nowait -chardev socket,id=compare0-0,host=3.3.3.3,port=9001 -chardev socket,id=compare_out,host=3.3.3.3,port=9005,server,nowait -chardev socket,id=compare_out0,host=3.3.3.3,port=9005 -object filter-mirror,id=m0,netdev=hn0,queue=tx,outdev=mirror0 -object filter-redirector,netdev=hn0,id=redire0,queue=rx,indev=compare_out -object filter-redirector,netdev=hn0,id=redire1,queue=rx,outdev=compare0 -object colo-compare,id=comp0,primary_in=compare0-0,secondary_in=compare1,outdev=compare_out0"
+
+cmdline="x86_64-softmmu/qemu-system-x86_64 -enable-kvm -boot c -m 2048 -smp 2 -qmp stdio -vnc :7 -name primary -cpu qemu64,+kvmclock -device piix3-usb-uhci -device usb-tablet $net_param -drive if=virtio,id=colo-disk0,driver=quorum,read-pattern=fifo,vote-threshold=1,children.0.file.filename=$disk_path,children.0.driver=raw -S"
+
+#exec $cmdline
+gdb --args $cmdline
---
+++ secondary-colo-v2.7-with-rewriter.sh
@@ -0,0 +1,13 @@
+echo "{'execute':'qmp_capabilities'}
+{'execute': 'nbd-server-start', 'arguments': {'addr': {'type': 'inet', 'data': {'host': '3.3.3.8', 'port': '8889'} } } }
+{'execute': 'nbd-server-add', 'arguments': {'device': 'colo-disk0', 'writable': true } }
+{'execute': 'trace-event-set-state', 'arguments': {'name': 'colo*', 'enable': true} }"
+
+disk_path=/home/cheng/ubuntu14.04.raw,driver=raw
+
+net_param="-netdev tap,id=hn0,vhost=off,script=/etc/qemu-ifup,downscript=/etc/qemu-ifdown -device e1000,netdev=hn0 -chardev socket,id=red0,host=3.3.3.3,port=9003,reconnect=1 -chardev socket,id=red1,host=3.3.3.3,port=9004,reconnect=1 -object filter-redirector,id=f1,netdev=hn0,queue=tx,indev=red0 -object filter-redirector,id=f2,netdev=hn0,queue=rx,outdev=red1 -object filter-rewriter,id=rew0,netdev=hn0,queue=all"
+
+cmdline="x86_64-softmmu/qemu-system-x86_64 -boot c -m 2048 -smp 2 -qmp stdio -vnc :7 -name secondary -enable-kvm -cpu qemu64,+kvmclock -device piix3-usb-uhci -device usb-tablet $net_param -drive if=none,id=colo-disk0,file.filename=$disk_path,node-name=node0 -drive if=virtio,id=active-disk0,driver=replication,mode=secondary,file.driver=qcow2,top-id=active-disk0,file.file.filename=/mnt/ramfs/active_disk.img,file.backing.driver=qcow2,file.backing.file.filename=/mnt/ramfs/hidden_disk.img,file.backing.backing=colo-disk0 -incoming tcp:0:8888"
+
+#exec $cmdline
+gdb --args $cmdline
